2c77c736f629cb755bcfdee1019b4e3228401ae5
Marina Sergeyeva <morrrinad@gmail.com>
2020-07-22 19:53:03 +0300

    navlink done

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,16 +1,41 @@
-const navigationListMarkup = (placeForInsert, data) => {
-  return `
-        <ul class="navigation__list">
-        <li class="navigation__item"></li>
-      </ul>
-    `;
+import './navigation.css';
+
+const navigation = (placeForInsert, elements) => {
+  const navigationListMarkup = elements => {
+    return `
+          <ul class="navigation__list">
+          ${navigationItemMarkup(elements)}
+          </ul>
+          `;
+  };
+
+  const navigationItemMarkup = elements => {
+    return elements.reduce((acc, element) => {
+      acc += `
+                <li class="navigation__item" data-navigationlink="${element}">
+                <a href="#">${element}</a>
+                </li>
+                `;
+
+      return acc;
+    }, '');
+  };
+
+  placeForInsert.innerHTML = navigationListMarkup(elements);
 };
 
-const navigationItemMarkup = (placeForInsert, data) => {
-  return `
-        <li class="navigation__item"></li>
-    `;
+export const setActiveLink = e => {
+  e.preventDefault();
+  if (
+    e.target !== e.currentTarget &&
+    (e.target.nodeName === 'LI' || e.target.nodeName === 'A')
+  ) {
+    console.dir(e.target);
+    const activeLink = e.target.dataset.navigationlink
+      ? e.target.dataset.navigationlink
+      : e.target.closest('[data-navigationlink]').dataset.navigationlink;
+    console.log(activeLink);
+  } else return;
 };
 
-
-export default
+export default navigation;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import './styles.css';
-import navigation from './components/navigation';
+import navigation, { setActiveLink } from './components/navigation';
 
-const data = ['Home', 'Products', 'Contacts', 'About', 'Cart'];
+const elementsNav = ['Home', 'Products', 'Contacts', 'About', 'Cart'];
+// const elementsCategories = ['Bread', 'Fruits', 'Vegetables', 'Meat', 'Fish'];
 
-navigation();
+const navigation_header = document.querySelector('.navigation_header');
+const navigation_main = document.querySelector('.main');
+
+const nav = document.querySelector('.navigation_header');
+const main = document.querySelector('main');
+
+navigation(navigation_header, elementsNav);
+// navigation(navigation_main, elementsCategories);
+
+nav.addEventListener('click', setActiveLink);
