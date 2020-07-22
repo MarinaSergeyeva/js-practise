ba120cbf9011e9d237ba3a4e272b351292db9a2d
Marina Sergeyeva <morrrinad@gmail.com>
2020-07-22 16:51:19 +0300

    navigation

---
+++ src/components/navigation.js
@@ -0,0 +1,16 @@
+const navigationListMarkup = (placeForInsert, data) => {
+  return `
+        <ul class="navigation__list">
+        <li class="navigation__item"></li>
+      </ul>
+    `;
+};
+
+const navigationItemMarkup = (placeForInsert, data) => {
+  return `
+        <li class="navigation__item"></li>
+    `;
+};
+
+
+export default
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './styles.css';
+import navigation from './components/navigation';
+
+const data = ['Home', 'Products', 'Contacts', 'About', 'Cart'];
+
+navigation();
